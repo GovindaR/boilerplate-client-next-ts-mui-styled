4df075222689ac7d1bd4e323523e82e34859d0f0
Govinda <govindaranamagar44@gmail.com>
2022-01-31 16:00:12 +0545

    restructered file

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import type { NextPage } from "next";
-import { Login } from "../src/modules/_core/components/Login";
+import React from 'react';
+import type { NextPage } from 'next';
+import { Login } from '../src/modules/_core/components/Login';
 
 const LoginPage: NextPage = () => {
   return <Login />;
